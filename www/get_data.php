2b8955caada073194a57db9cae59a371a93a3e1e
Daniel García <daniegarcia254@gmail.com>
2015-08-03 23:21:55 +0000

    Deleted sensitive data

--- www/get_data.php
+++ www/get_data.php
@@ -15,30 +15,11 @@ function get_parse($params){
       CURLOPT_RETURNTRANSFER => 1,
       CURLOPT_URL => $url . '?' . $params,
       CURLOPT_USERAGENT => 'UserAgentString',
-      CURLOPT_HTTPHEADER => array('X-Parse-Application-ID: 80N6VQEMuBhOGV4F8TfzqnXnXlrEx9bSOQqhrMM6',
-                                  'X-Parse-REST-API-Key: 2F3RBbLIBLppo6EVVOZ52bTPbcdAIMJMGbKqFq4s')
+      CURLOPT_HTTPHEADER => array('X-Parse-Application-ID: XXXX',
+                                  'X-Parse-REST-API-Key: XXXX')
     )
   );
   $resp = curl_exec($curl);
   curl_close($curl);
   return $resp;
 }
-
-
-/*if (isset($_GET['tipo'])) {
-  $params = array('where' => '{"tipo":"' . $_GET['tipo'] . '"}');
-}
-else{
-  die;
-}
-
-$resp=get_parse($params);
-
-header('Content-Type: application/json');
-$json = json_decode($resp);
-print(json_encode($json,JSON_PRETTY_PRINT));
-*/
-
-// Obtener predicciones
-// curl -X GET -H "X-Parse-Application-ID: 80N6VQEMuBhOGV4F8TfzqnXnXlrEx9bSOQqhrMM6" -H "X-Parse-REST-API-Key: 2F3RBbLIBLppo6EVVOZ52bTPbcdAIMJMGbKqFq4s"  -G --data-urlencode 'where={"tipo":"prediccion"}' https://api.parse.com/1/classes/Info_Object
-
