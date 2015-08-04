984ca533525e01d31e6e297de224fe04a40a0df8
Daniel García Páez <daniegarcia254@gmail.com>
2015-08-04 21:03:54 +0200

    Update graphs.php

--- www/templates/graphs.php
+++ www/templates/graphs.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset=utf-8 />
-	<title>BiZi-Meteo Graphs && Stats</title>
+	<title>BiZi-Meteo Graphs &amp;&amp; Stats</title>
         <link rel="stylesheet" type="text/css" href="../css/style.css">
         <script src="../js/chart.min.js"></script>
         <script src="../js/chart.extension.js"></script>
